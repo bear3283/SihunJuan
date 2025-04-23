cfd26f74c2fd2b8359185fa7388b11126e64a5a2
bear <duilwang@naver.com>
2025-04-23 18:16:50 +0900

    opacity changes

--- SihunJuan/ContentView.swift
+++ SihunJuan/ContentView.swift
@@ -30,7 +30,7 @@ struct TapTheDotGameView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [Color.white, Color.yellow.opacity(0.3)]),
+                gradient: Gradient(colors: [Color.white, Color.yellow.opacity(1)]),
                 startPoint: .top,
                 endPoint: .bottom
             )
